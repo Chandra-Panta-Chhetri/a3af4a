060cd3c2f839399d0dfd498dc8459e4965516fce
Chandra Panta C <chandra.panta345@hotmail.com>
2022-02-11 11:24:40 -0500

    Fixes read status test suite.

--- client/cypress/integration/read-status-ticket.spec.js
+++ client/cypress/integration/read-status-ticket.spec.js
@@ -1,38 +1,38 @@
 /// <reference types="cypress" />
 
-const alice = {
-    username: "Alice",
-    email: "alice@example.com",
+const user1 = {
+    username: "Chandra",
+    email: "chandra@example.com",
     password: "Z6#6%xfLTarZ9U",
 };
 
-const bob = {
-    username: "Bob",
-    email: "bob@example.com",
+const user2 = {
+    username: "Zack",
+    email: "zack@example.com",
     password: "L%e$xZHC4QKP@F",
 };
   
 describe("Read Status", () => {
     it("setup", () => {
-        cy.signup(alice.username, alice.email, alice.password);
+        cy.signup(user1.username, user1.email, user1.password);
         cy.logout();
-        cy.signup(bob.username, bob.email, bob.password);
+        cy.signup(user2.username, user2.email, user2.password);
         cy.logout();
     });
 
     it("shows unread count for new messages", () => {
-        cy.login(alice.username, alice.password);
+        cy.login(user1.username, user1.password);
 
-        cy.get("input[name=search]").type("Bob");
-        cy.contains("Bob").click();
+        cy.get("input[name=search]").type(user2.username);
+        cy.contains(user2.username).click();
 
         cy.get("input[name=text]").type("First message{enter}");
         cy.get("input[name=text]").type("Second message{enter}");
 
         cy.logout();
-        cy.login(bob.username, bob.password);
+        cy.login(user2.username, user2.password);
 
-        //Unread message count for Bob
+        //Unread message count for user 2
         cy.contains("2");
     });
 });
