51cd796dcb2ba7615dc5a23b68dbc17f24571f01
Chandra Panta C <chandra.panta345@hotmail.com>
2022-02-16 13:10:49 -0500

    Renames last read model to group and msg field to lastReadMsg.

--- server/messenger_backend/models/group.py
+++ server/messenger_backend/models/group.py
@@ -7,13 +7,13 @@ from .message import Message
 from .conversation import Conversation
 
 
-class LastRead(utils.CustomModel):
+class Group(utils.CustomModel):
 
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, db_column="userId", related_name="+"
     )
-    msg = models.ForeignKey(
-        Message, on_delete=models.CASCADE, db_column="msgId", related_name="+"
+    lastReadMsg = models.ForeignKey(
+        Message, on_delete=models.CASCADE, db_column="lastReadMessageId", related_name="+"
     )
     conversation = models.ForeignKey(
         Conversation, on_delete=models.CASCADE, db_column="conversationId", related_name="+"
